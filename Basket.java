19fe71359a758f3a147929c40de3951a586afd1c
charbelkhabbaz <ckhabbaz7@gmail.com>
2023-02-09 21:41:53 +0200

    Basket

---
+++ Basket.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Basket{
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		int players,gamers;
+	System.out.print("give the numbers of players then gamers ");
+	players = scan.nextInt();
+	gamers= scan.nextInt();
+	}
+}
